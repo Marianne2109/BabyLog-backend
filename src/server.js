42ca88884780bcf761c2d9dbb5d86b698d1d1434
Marianne Veyl Neumann <15081@coderacademy.edu.au>
2025-01-03 02:30:49 +1100

    Add API root response

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 
-
-
+//make server instance
 const app = express();
 
 //SERVER CONFIGURATION
 
 
-
+//API response - app.verb(path, callback);
+app.get("/", (request, response) => {
+   
+    response.json({
+        message: "BabyLog"
+    });
+});
 
 
 
