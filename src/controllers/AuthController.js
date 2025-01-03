5dfe87d88d676fa9da7761128cd42854bdb6f8ac
Marianne Veyl Neumann <15081@coderacademy.edu.au>
2025-01-04 04:43:09 +1100

    WIP AuthController setup

---
+++ src/controllers/AuthController.js
@@ -0,0 +1,32 @@
+//Auth controller will handle registration and login
+
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/User")
+
+class AuthController {
+    //Registration 
+    async register(req, res){
+        try {
+            const {email, password, firstName, lastName, role} = req.body;
+
+            //Check if user with email exists
+            const existingUser = await User.findOne({email: email.toLowerCase()});
+            if (existingUser) {
+                return res.status(400).json({
+                    error: "It looks like this email is already registered, try again with a different email."
+                });
+            }
+        }
+
+        //Create new user if email not registered already
+        const user = new User({
+            email: email.toLowerCase(),
+            firstName,
+            lastName,
+            password,
+            role: role || "parent" //set parent as default role if not specified
+        });
+
+        await user.save();
+    }
+}
