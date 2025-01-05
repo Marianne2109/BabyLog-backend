06150abe88cf8efb6b81ed6f824dad8084787971
Marianne Veyl Neumann <15081@coderacademy.edu.au>
2025-01-06 03:45:47 +1100

    fixed small typo error

--- src/utils/database.js
+++ src/utils/database.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const { UserModel } = require("../models/UserModel");
 
 async function dbConnect(){
-    let databaseUrl = process.env.DATABSE_URL || `mongodb://127.0.0.1:27017/${process.env.npm_package_name}`;
+    let databaseUrl = process.env.DATABASE_URL || `mongodb://127.0.0.1:27017/${process.env.npm_package_name}`;
 
     await mongoose.connect(databaseUrl);
 }
