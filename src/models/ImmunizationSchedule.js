790a3aef8e54a0a1b0ad76debebe23ae9f8ed6cf
Marianne Veyl Neumann <15081@coderacademy.edu.au>
2025-01-20 14:30:07 +1100

    update nipSchedule.json and immunizationschedule model to fix issues with schema and seeding

--- src/models/ImmunizationSchedule.js
+++ src/models/ImmunizationSchedule.js
@@ -1,57 +1,61 @@
 const mongoose = require("mongoose");
 
+//Nested schema for vaccines
 const VaccineSchema = new mongoose.Schema({
     diseaseName: { 
         type: String,
         required: true,
+        trim: true,
     },
     vaccineBrand: {
         type: String,
         default: null,
+        trim: true,
     },
     notes: {
         type: String,
         default: null,
-    }
+        trim: true,
+    },
 });
 
+//Main schema for immunization schedule
 const ImmunizationScheduleSchema = new mongoose.Schema({
     id: {
         type: Number,
         required: true,
+        unique: true,
     },
     age: {
         type: String,
         required: true,
+        trim: true
     },
-    vaccines: [VaccineSchema],
+    vaccines: {
+        type: [VaccineSchema],
+        required: true,
+    },
+    notes: {
+        type: String,
+        default: null,
+        trim: true,
+    },    
 });
 
-//     diseaseName: {
-//         type: String,
-//         required: true,
-//     },
-//     vaccineBrand: {
-//         type: String,
-//     },
-//     notes: {
-//         type: String,
-//     },
-// });
 
-const getFullSchedule = async () => {
-    return await ImmunizationSchedule.find();
-};
+// const getFullSchedule = async () => {
+//     return await ImmunizationSchedule.find();
+// };
 
-const getScheduleByAge = async (age) => {
-    return await ImmunizationSchedule.findOne({ age });
-};
+// const getScheduleByAge = async (age) => {
+//     return await ImmunizationSchedule.findOne({ age });
+// };
 
 
 const ImmunizationSchedule = mongoose.model('ImmunizationSchedule', ImmunizationScheduleSchema);
 
 module.exports  = {
     ImmunizationSchedule,
-    getFullSchedule,
-    getScheduleByAge
+    // getFullSchedule,
+    // getScheduleByAge
 }
