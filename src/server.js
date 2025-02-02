9f7fdc322324d1ac50d46b5c53c3ae27912d4c59
Marianne Veyl Neumann <15081@coderacademy.edu.au>
2025-02-02 21:34:33 +1100

    update server.js

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 
 //Enables cross-origin resource sharing
 const corsOptions = {
-    origin: ["http://127.0.0.1:5174", "http://localhost:5174", "http://localhost:3000", "https://babylog-backend.onrender.com/"],
-    methods: ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE"],
+    origin: ["http://127.0.0.1:5174", "http://localhost:5174", "http://127.0.0.1:5173", "http://localhost:5173", "http://localhost:3000", "https://babylog-backend.onrender.com/"],
+    methods: ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     optionsSuccessStatus: 200,
     credentials: true
